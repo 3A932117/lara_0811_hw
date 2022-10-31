e149b035ba3ad104f477a0d440e906863b630d25
Huang Jia Yi <3a932117@gm.student.ncut.edu.tw>
2022-10-31 20:23:04 +0800

    產生Controller並設定路由

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\NewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/about', [AboutController::class, 'index'])->name('about.index');
+Route::get('/news', [NewsController::class, 'index'])->name('news.index');
 
 Route::get('/', function () {
     return view('welcome');
